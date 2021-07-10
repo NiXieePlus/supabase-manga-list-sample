21d83e30509261c89073fb4a7a78d4c5d5bb4a5b
Liebe <magical.liebe@gmail.com>
2021-07-10 13:26:43 +0900

    💄 faviconを追加

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta
+            name="description"
+            content="漫画の所持状況確認アプリ"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
